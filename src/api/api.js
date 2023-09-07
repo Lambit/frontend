fef4520b8da11640b01622cbfd05c1f4205bb3b2
Lambit <luke.lamb.audio@gmail.com>
2023-09-07 13:31:44 -0400

    config for deployment

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import i18n from '../locale/i18';
 import storage from "../storage/storage";
 
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000";
+
 //Runs heads on every request
 axios.interceptors.request.use((request) => {
     request.headers['Accept-Language'] = i18n.language;
@@ -18,33 +20,33 @@ axios.interceptors.request.use((request) => {
 
 
 export const signUp = (body) => { 
-    return axios.post('/api/users', body);
+    return axios.post(`${BASE_URL}/api/users`, body);
 };
 
 export const activate = (token) => {
-    return axios.post('/api/users/token', + token);
+    return axios.post(`${BASE_URL}/api/users/token`, + token);
 };
 
 export const loadUsers = (page) => {
-    return axios.get('/api/users', {params : { page , size: 3 } });
+    return axios.get(`${BASE_URL}/api/users`, {params : { page , size: 3 } });
 };
 
 export const getUserById = (id) => {
-    return axios.get(`/api/users/${id}`);
+    return axios.get(`${BASE_URL}/api/users/${id}`);
 };
 
 export const login = (body) => {
-    return axios.post(`/api/auth`, body);
+    return axios.post(`${BASE_URL}/api/auth`, body);
 };
 
 export const logout = (body) => {
-    return axios.post(`/api/auth`, body);
+    return axios.post(`${BASE_URL}/api/auth`, body);
 };
 
 export const updateUser = (id, body) => {
-    return axios.put(`/api/users/${id}`, body);
+    return axios.put(`${BASE_URL}/api/users/${id}`, body);
 };
 
 export const deleteUser = (id) => {
-    return axios.delete(`/api/users/${id}`);
+    return axios.delete(`${BASE_URL}/api/users/${id}`);
 };
